39f7d09a295f722b0e6d471fd4eb6092519f3ea4
Tatyana404 <kot.kate2020@gmail.com>
2024-03-14 17:33:18 +0100

    add constants

---
+++ src/constants/index.ts
@@ -0,0 +1,20 @@
+const tags = [
+  { label: "CLIENT_FIRST_NAME", value: "CLIENT_FIRST_NAME" },
+  { label: "CLIENT_LAST_NAME", value: "CLIENT_LAST_NAME" },
+  { label: "STAFF_FIRST_NAME", value: "STAFF_FIRST_NAME" },
+  { label: "STAFF_LAST_NAME", value: "STAFF_LAST_NAME" },
+  { label: "BOOKING_DATE_TIME", value: "BOOKING_DATE_TIME" },
+  { label: "BOOKING_DATE", value: "BOOKING_DATE" },
+  { label: "BOOKING_TIME", value: "BOOKING_TIME" },
+  { label: "SERVICE_NAME", value: "SERVICE_NAME" },
+  { label: "BUSINESS_NAME", value: "BUSINESS_NAME" },
+  { label: "BUSINESS_PHONE", value: "BUSINESS_PHONE" },
+  { label: "BUSINESS_ADDRESS", value: "BUSINESS_ADDRESS" },
+  { label: "BUSINESS_EMAIL", value: "BUSINESS_EMAIL" },
+  { label: "LOCATION_NAME", value: "LOCATION_NAME" },
+  { label: "LOCATION_PHONE", value: "LOCATION_PHONE" },
+  { label: "LOCATION_ADDRESS", value: "LOCATION_ADDRESS" },
+  { label: "LOCATION_EMAIL", value: "LOCATION_EMAIL" },
+];
+
+export default tags;
